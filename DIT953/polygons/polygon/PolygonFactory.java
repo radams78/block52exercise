637c3c46215d918fd0968404ec9bbf8e668ae198
niklasbroberg <niklas.broberg@gmail.com>
2019-02-19 21:36:27 +0100

    Add files via upload

---
+++ DIT953/polygons/polygon/PolygonFactory.java
@@ -0,0 +1,14 @@
+package DIT953.polygons.polygon;
+
+/**
+ * Created by Niklas on 2016-02-19.
+ */
+public class PolygonFactory {
+
+    public static IPolygon createTriangle(int x, int y){
+        return new Triangle(x,y);
+    }
+    public static IPolygon createRectangle(int x, int y) { return new Rectangle(x,y); }
+    public static IPolygon createSquare(int x, int y) { return new Square(x,y); }
+
+}
